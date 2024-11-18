728b079b5963c6e6138e512e7579a8a8b0f33686
Kili <kilimanjaro.a2@gmail.com>
2024-11-19 00:51:15 +0900

    ページやタブ移動でデカモードをOFFにする

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,20 @@ import { callLlm, isApiKeyRequired } from "./requester";
 import { retrieveTextInjection, replaceTextInjection, restoreOriginalTextsInjection, insertCssInjection, registerAlert } from "./injection";
 
 
+chrome.tabs.onActivated.addListener((activeInfo) => {
+  chrome.tabs.get(activeInfo.tabId, (_) => {
+    chrome.action.setBadgeText({ text: "OFF" });
+  });
+});
+
+chrome.webNavigation.onCompleted.addListener((details) => {
+  if (details.frameId === 0) {
+    chrome.tabs.get(details.tabId, (_) => {
+      chrome.action.setBadgeText({ text: "OFF" });
+    });
+  }
+});
+
 
 chrome.action.onClicked.addListener(async (tab) => {
   if (tab.id === undefined) {
